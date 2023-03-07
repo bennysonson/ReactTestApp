6ebc60d2144a70063b6d83a6d49a24d1ee7a5f92
Benson Liu <epiccowslayer@gmail.com>
2023-03-07 12:09:13 -0500

    Added static movie view

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import {useEffect} from 'react';
+import './App.css'
+import searchIcon from './search.svg'
+import MovieCard from "./MovieCard";
 
 //6d18a697
 
 const API_URL = 'http://www.omdbapi.com?apikey=6d18a697'
 
 const App = () => {
+
+    const movie1 = {
+        "Title": "Spiderman",
+        "Year": "1990",
+        "imdbID": "tt0100669",
+        "Type": "movie",
+        "Poster": "N/A"
+    }
 
     const searchMovies = async(title) => {
         const response = await fetch(`${API_URL}&s=${title}`)
@@ -18,7 +29,17 @@ const App = () => {
     }, []);
 
     return (
-        <h1>hi</h1>
+        <div className="App">
+            <h1>MovieList</h1>
+            <div className="search">
+                <input placeholder="Search for Movies" value="" onChange={() => {}}>
+                </input>
+                <img src={searchIcon} alt="Search" onClick={() => {}}></img>
+            </div>
+            <div className="container">
+                <MovieCard movie={movie1}></MovieCard>
+            </div>
+        </div>
     );
 }
 
